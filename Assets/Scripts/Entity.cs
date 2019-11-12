dc885a49cdcc9f2cd3cc46c4e434e49172c4fc1d
Thomas RIchards <thomas.c.richards1@gmail.com>
2019-11-12 20:21:05 +0100

    Replaced old movement/playerMovement scripts with entity and player entity scripts.

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    //Speed & Direction
+    protected enum States { Standing, Walking, Ducking, Jumping };
+
+    protected States mState;
+    [Range(0.0f, 100.0f)]
+    [SerializeField] protected float mVelocity;
+    protected Vector2 mJumpPower;
+    protected Transform mTransform;
+    protected Transform mGroundCheck;
+    protected Collider2D mGroundCheckCol;
+    protected Animator mAnimator;
+    protected bool mIsGrounded;
+    protected SpriteRenderer mSprite;
+    protected Collider2D mCollider;
+    protected GameObject mGameObject;
+    protected Vector2 mForce;
+
+    protected float mDirection;
+    protected float mGravity = 0.0f;
+    protected float mGravityDirection = -1.0f;
+    protected Rigidbody2D mRigidbody;
+
+    protected Vector2Int mPos;
+
+    virtual protected void Start()
+    {
+
+    }
+    virtual protected void Update()
+    {
+        
+    }
+    public void InitializeMovement()
+    {
+        mSprite = GetComponent<SpriteRenderer>();
+        mAnimator = GetComponent<Animator>();
+        mTransform = GetComponent<Transform>();
+        mGroundCheck = GetComponentInChildren<Transform>();
+        mGroundCheck.localPosition.Set(0f, mSprite.size.y, 0f);
+        mGroundCheckCol = mGroundCheck.gameObject.GetComponent<Collider2D>();
+    }
+    private void CalculateNewForce()
+    {
+        mForce.x = mVelocity * mDirection;
+    }
+    protected void Move()
+    {
+        CalculateNewForce();
+        mRigidbody.AddForce(mForce, ForceMode2D.Force);
+    }
+    protected void Jump()
+    {
+        mRigidbody.AddForce(mJumpPower, ForceMode2D.Impulse);
+    }
+    protected void Turn()
+    {
+        if(mDirection < 0)
+        {
+            mSprite.flipX = false;
+        }
+        else if (mDirection > 0)
+        {
+            mSprite.flipX = true;
+        }
+    }
+    public SpriteRenderer GetSpriteRenderer()
+    {
+        return mSprite;
+    }
+    public Transform GetTransform()
+    {
+        return mTransform;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        mIsGrounded = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        mIsGrounded = false;
+    }
+}
---
+++ Assets/Scripts/PlayerEntity.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEntity : Entity
+{
+    enum PowerUp {Small, Big, Fireflower}
+    
+    PowerUp mPowerUp;
+    private KeyboardInput playerInput;
+    private float mVelocityMax;
+    private bool mIsDucking;
+    protected override void Start()
+    {
+        mJumpPower.x = 0.0f;
+        mJumpPower.y = 8.0f;
+        mVelocityMax = mVelocity + 5.0f;
+        //playerInput = GetComponent<KeyboardInput>();
+        mRigidbody = GetComponent<Rigidbody2D>();
+    }
+    protected override void Update()
+    {
+        mDirection = GetDirection();
+        mAnimator.SetFloat("Speed", Mathf.Abs(mDirection));
+        mAnimator.SetBool("Grounded", mIsGrounded);
+        mAnimator.SetBool("Down", mIsDucking);
+        if(mIsDucking == false)
+        {
+            Move();
+        }
+        
+        Turn();
+        if (Input.GetKey(KeyCode.DownArrow) && mIsGrounded == true)
+        {
+            Duck(true);
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            Duck(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && mIsGrounded == true)
+        {
+            Jump();
+        }
+        
+ 
+    }
+    float GetDirection()
+    {
+        float inputDirection = 0.0f;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            inputDirection = -1f;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            inputDirection = 1f;
+        }
+        return inputDirection;
+    }
+    private void Duck(bool state)
+    {
+        mIsDucking = state;
+    }
+    private void SetState(States state)
+    {
+        mState = state;
+    }
+    private void SetState(PowerUp powerUp)
+    {
+        mPowerUp = powerUp;
+    }
+}
